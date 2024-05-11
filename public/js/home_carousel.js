22f64089e4f7c1cbf9f46f9b96399128ab428857
Camille Cajas <ccajas001@gmail.com>
2024-05-11 02:23:14 -0400

    css home page edits

--- public/js/home_carousel.js
+++ public/js/home_carousel.js
@@ -4,12 +4,11 @@ const imgs = document.getElementById('imgs');
 const leftbtn = document.getElementById('left');
 const rightbtn = document.getElementById('right');
 const playpausebtn = document.getElementById('playPause');
-
-const img = document.querySelectorAll('#imgs .champ-profile');
-
+const carasoulsection =document.querySelector('.latest-champ-carousel');
+const img= document.querySelectorAll('#imgs .champ-img');
 // set up inital place,interval,flag for if the carousel is playing
 let idx = 0;
-let interval = setInterval(run, 2000)
+let interval = setInterval(run, 3000)
 let isPlaying = true;
 
 function run(){
@@ -25,6 +24,10 @@ function changeImage(){
         idx = img.length-1;
     }
     imgs.style.transform=`translateX(${-idx*60}vw)`;
+    let imageURL =img[idx].src;
+    console.log("url('"+imageURL.substring(21)+"')");
+    carasoulsection.style.backgroundImage="linear-gradient( rgba(15, 15, 15, 0.747), rgba(115, 115, 115, 0.49)), url('.."+imageURL.substring(21)+"')";
+
 }// change image
 
 function resetInterval(){
